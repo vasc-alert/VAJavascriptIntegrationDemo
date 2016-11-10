707e0aafeb4cf2348ffe55107866fd4444f6e720
Larry Mills-Gahl <elemgee@gmail.com>
2016-11-10 10:29:31 -0600

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        filename: "va-integration.js"
+    },
+    module: {
+        loaders: [
+            { exclude: /node_modules/}
+        ]
+    }
+};
